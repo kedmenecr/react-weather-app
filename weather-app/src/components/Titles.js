b20c1762a94ee25457f599f524d93a301660b124
Roberto <kedmenec.roberto@gmail.com>
2018-06-10 15:16:03 +0200

    Add: Styling updates.

--- weather-app/src/components/Titles.js
+++ weather-app/src/components/Titles.js
@@ -1,14 +1,11 @@
 import React from "react";
 
-class Titles extends React.Component{
-    render(){
-        return(
-            <div>
-            <h1>Weather finder</h1>
-            <p>Find out temperature connditions and more!</p>
-            </div>
+const Titles = () => {
+    return (
+        <div>
+            <h1 className="title-container__title">Weather finder</h1>
+            <p className="title-container__subtitle">Find out temperature conditions and more!</p>
+        </div>
         );
-    }
 }
-
 export default Titles;
